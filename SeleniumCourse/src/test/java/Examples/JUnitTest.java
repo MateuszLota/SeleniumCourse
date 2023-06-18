e2cd2dacd18273405c179b6896e267f288b2f034
Mateusz <mateuszlota112@gmail.com>
2023-06-18 11:23:44 +0200

    Junit test

---
+++ SeleniumCourse/src/test/java/Examples/JUnitTest.java
@@ -0,0 +1,11 @@
+package Examples;
+
+import org.junit.jupiter.api.Test;
+
+
+public class JUnitTest {
+    @Test
+    public void FirstTest(){
+        System.out.println("Test uruchomiony");
+    }
+}
